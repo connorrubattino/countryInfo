7b3b53db62b7f8ea6cd9d82f3cd7192c012dddb9
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-14 18:57:48 -0700

    functional but card is there the whole time, cant search multiple times just once

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
+document.getElementById('country-button').addEventListener('click', findCountry);
 
-//find the country
+// //find the country
 function findCountry(e){
     e.preventDefault(); //prevent page from refreshing w/ data
     let countryName = document.getElementById('country-input')?.value;
     const url = `https://restcountries.com/v3.1/name/${countryName}`
+    console.log(url);
     fetch(url)
-    .then( res => res.json() )
-    .then( data => showCountry() ) 
-    .catch( err => console.error(err) )
+        .then( res => res.json() )
+        .then( data => showCountry(data) ) 
+        .catch( err => console.error(err) )
 }
 
 //show country function will take in country data and insert into card
 function showCountry(data){
+    if(data && data.length > 0){
+        const toTurnOn = document.getElementsByClassName('is-invisible')[0];
+        toTurnOn.classList.replace('is-invisible', 'is-visible');
+    }
+
     let searchedCountry = data[0]
     if (!searchedCountry){
-        console.log('We had trouble fetching your data - Please try a different spelling or different country.')
+        console.log('We had trouble fetching your data - Please try a different spelling or different country.');
+        return;
     }
-    let countryName = searchedCountry[name][common]
-    let countryCapital = searchedCountry[capital]
-    let countryFlag = searchedCountry[flags][png]
+    let countryName = searchedCountry['name']['common']
+    let countryCapital = searchedCountry['capital']
+    let countryFlag = searchedCountry['flags']['png']
 
-    let countryLangList = Object.values(searchedCountry[languages])
-    let countryLanguages = Object.values(searchedCountry[languages][0])
-    if (countryLangList.length > 1){
-        for (let language of countryLangList){
-            countryLanguages += `, ${language}`
-        }
+    let countryLangList = Object.values(searchedCountry['languages']);
+    let countryLanguages;
+    if (countryLangList.length === 1){
+        countryLanguages = countryLangList[0];
+    } else {
+        countryLanguages = countryLangList.join(', ');
     }
 
-    let countryCurrList = Object.values(searchedCountry[currencies])
-    let countryCurrencies = Object.values(searchedCountry[currencies][0][name])
-    if (countryCurrList.length > 1){
-        for (let currency of countryCurrList){
-            countryCurrencies += `, ${currency[name]}`
-        }
+    let countryCurrList = Object.values(searchedCountry['currencies']);
+    let countryCurrencies
+    if (countryCurrList.length === 1) {
+        countryCurrencies = countryCurrList[0]['name'];
+    } else {
+        countryCurrencies = countryCurrList.map(currency => currency['name']).join(', ');
     }
 
     document.getElementById('country-name').innerHTML = `${countryName}`
-    document.getElementById('country-currency').innerHTML = `${countryCurrencies}`
-    document.getElementById('country-capital').innerHTML = `${countryCapital}`
-    document.getElementById('country-languages').innerHTML = `${countryLanguages}`
+    document.getElementById('country-currency').innerHTML = `Currencies: ${countryCurrencies}`
+    document.getElementById('country-capital').innerHTML = `Capital: ${countryCapital}`
+    document.getElementById('country-languages').innerHTML = `Language(s):${countryLanguages}`
     document.getElementById('country-image').src = `${countryFlag}`
 }
 
-let countrySearchBar = document.getElementById('country-input');
-countrySearchBar.addEventListener('submit', e => findCountry(e))
+// let countrySearchBar = document.getElementById('country-enter');
+// countrySearchBar.addEventListener('submit', e => findCountry(e));
